f5dad0372359a3184b67c6d4bd27ab86349c4d6c
Jack Tierney <jack.tierney@student.ucc.ie>
2023-08-02 12:36:29 +0100

    added stop stop orf search

--- findOverlapping.py
+++ findOverlapping.py
@@ -29,6 +29,54 @@ def max_consecutive(arr):
     
     return longest
 
+def get_stop_codon_locations(mrna_seq):
+    '''
+    Return a codon array of stop codon positions. One digit per codon
+    '''
+    stop_codons = ["TAA", "TAG", "TGA"]
+
+        # initialise array to store where stop codons are found. 0 for none 1 for found
+    stop_codon_frames = [[],[],[]]
+    for i in range(len(mrna_seq)):
+        if mrna_seq[i:i+3] in stop_codons:
+            stop_codon_frames[i%3].append(1)
+        else:
+            stop_codon_frames[i%3].append(0)
+
+    return stop_codon_frames
+
+
+
+def find_longest_sequence_w_no_stop_codon(stop_codon_frames):
+    '''
+    Find the longest sequence of 0s in the stop codon array
+
+    Input:
+        stop_codon_frames: array of 0s and 1s
+
+    Output:
+        longest_sequence: integer of the longest sequence of 0s in the array
+    '''
+    tup_list = [(stop_codon_frames[0][i], stop_codon_frames[1][i], stop_codon_frames[2][i]) for i in range(len(stop_codon_frames[2]))]
+
+    longest_sequence = 0
+    current_sequence = 0
+    position = [0, 0]
+    max_position = [0, 0]
+
+    for i in enumerate(tup_list):
+        if i[1] == (0,0,0):
+            current_sequence += 1
+        else:
+            position[1] = i[0]
+            if current_sequence > longest_sequence:
+                max_position = position
+                longest_sequence = current_sequence
+            current_sequence = 0
+            position = [i[0], i[0]]
+
+    return longest_sequence, max_position[0], max_position[1]
+
 
 def find_longest_overlapping_open_reading_length(mrna):
     '''
@@ -41,21 +89,11 @@ def find_longest_overlapping_open_reading_length(mrna):
         longest_orf_length: integer of the length of the longest ORF
     
     '''
-    stop_codons = ["TAA", "TAG", "TGA"]
-    longest_orf_length = 0
-
     mrna_seq = Seq(mrna)
-
-    # initialise array to store where stop codons are found. 0 for none 1 for found
-    stop_codon_frames = [[],[],[]]
-    for i in range(len(mrna_seq)):
-        if mrna_seq[i:i+3] in stop_codons:
-            stop_codon_frames[i%3].append(1)
-        else:
-            stop_codon_frames[i%3].append(0)
+    stop_codon_frames = get_stop_codon_locations(mrna_seq)
+    longest_3_frame = find_longest_sequence_w_no_stop_codon(stop_codon_frames)
 
     longest_orf = 0
-    longest_orfs = []
     frame1 = max_consecutive(stop_codon_frames[0])
     frame2 = max_consecutive(stop_codon_frames[1])
     frame3 = max_consecutive(stop_codon_frames[2])
@@ -94,7 +132,7 @@ def find_overlapping_regions(coordinates):
                 if list(interval)[:2] not in [i[1] for i in overlaps]:
                     overlaps.append((list(interval)[:2], list(other_interval)[:2], overlap_length))
         
-    print(overlaps)
+    return overlaps
 
 def find_longest_regions_from_fasta(file_path):
     results = []
@@ -102,21 +140,27 @@ def find_longest_regions_from_fasta(file_path):
     for record in SeqIO.parse(file_path, "fasta"):
         mrna_name = record.id
         mrna_sequence = record.seq
-        longest_region_length = find_longest_overlapping_open_reading_length(mrna_sequence)
-        coordinates = [
-            (longest_region_length[1], longest_region_length[2]),
-            (longest_region_length[4], longest_region_length[5]),
-            (longest_region_length[7], longest_region_length[8])
-        ]
-        find_overlapping_regions(coordinates)
+        stop_codon_frames = get_stop_codon_locations(mrna_sequence)
+        longest_3_frame = find_longest_sequence_w_no_stop_codon(stop_codon_frames)
+        results.append((mrna_name, longest_3_frame[0]))
+
+        # longest_region_length = find_longest_overlapping_open_reading_length(mrna_sequence)
+        # coordinates = [
+        #     (longest_region_length[1], longest_region_length[2]),
+        #     (longest_region_length[4], longest_region_length[5]),
+        #     (longest_region_length[7], longest_region_length[8])
+        # ]
+        # find_overlapping_regions(coordinates)
         
-        results.append((mrna_name, str(longest_region_length).strip("[]").replace(", ", "\t")))
-        if len(results) > 2:
-            return results
+        # results.append((mrna_name, str(longest_region_length).strip("[]").replace(", ", "\t")))
+        # if len(results) > 2:
+        #     return results
+    return results
 
 def write_results_to_tsv(results, output_file):
     with open(output_file, "w") as f:
-        f.write("Sequence_Name\tFrame 0 Max\tFrame 0 Start\tFrame 0 End\tFrame 1 Max\tFrame 1 Start\tFrame 1 End\tFrame 2 Max\tFrame 2 Start\tFrame 2 End\n")
+        # f.write("Sequence_Name\tFrame 0 Max\tFrame 0 Start\tFrame 0 End\tFrame 1 Max\tFrame 1 Start\tFrame 1 End\tFrame 2 Max\tFrame 2 Start\tFrame 2 End\n")
+        f.write("Sequence_Name\tLongest_3_Frame\n")
         for mrna_name, longest_length in results:
             f.write(f"{mrna_name}\t{longest_length}\n")
 
@@ -126,5 +170,4 @@ if __name__ == "__main__":
     fasta_file_path = "data/gencode.v44.transcripts.fa"
     output_tsv_file = "output_file.tsv"
     results = find_longest_regions_from_fasta(fasta_file_path)
-    print(results)
     write_results_to_tsv(results, output_tsv_file)
